091fdccde1b88ce1635e1e6f589c237e9f13fb0d
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-06-12 10:39:01 +0530

    Remove "Learn More" section from why-electric.php to streamline content presentation.

--- sections/why-electric.php
+++ sections/why-electric.php
@@ -41,10 +41,6 @@
                     <h3><?php echo htmlspecialchars($item['title']); ?></h3>
                     <p><?php echo htmlspecialchars($item['text']); ?></p>
                     
-                    <div class="card-link-arrow">
-                        <span>Learn More</span>
-                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" class="arrow-svg"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
-                    </div>
                 </div>
             </div>
             <?php } ?>
